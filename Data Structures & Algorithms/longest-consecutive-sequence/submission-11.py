161b2ff439a217dafe70e0d6b38084acf3c0425a
Andres Perez <92131116+andres-perez0@users.noreply.github.com>
2026-07-21 20:26:37 -0700

    Add: longest-consecutive-sequence - submission-11

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-11.py
@@ -0,0 +1,76 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # review
+        if not nums:
+            return 0
+            
+        max_length = 0
+        set_of_nums = set(nums)
+        list_of_length = []
+
+        for n in nums:
+            if (n - 1) not in set_of_nums:
+                cnt = 0
+                while ((n + cnt) in set_of_nums):
+                    cnt += 1
+                
+                list_of_length.append(cnt)
+        
+        for length in list_of_length:
+            if length > max_length:
+                max_length = length
+            
+        return max_length
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # # I imagine each element as a circle
+        # # first, its just [100] 
+        # # second, its just [100], [4]
+        # # third, its just [100], [4], [200]
+        # # fourth, its [100], [4], [200], [1]
+        # # fifth, its [100], [4], [200], [1], [3] -> [100], [3] -> [4], [200], [1]
+        # # sixth, its [100], [3] -> [4], [200], [1], [2] 
+        # ##### == its [100], [2] -> [3] -> [4], [200], [1]
+        # ##### == its [100], [1] -> [2] -> [3] -> [4], [200]
+        # # transforms the array into a set to get constant look up time
+
+        # if len(nums) <= 0:
+        #     return 0
+
+        # global_set_max = 0
+        # nums = set(nums)
+        # for n in nums:
+        #     # if there's no n - 1 the current n, there theres no way for it to be consecutive
+        #     if n - 1 not in nums or len(nums) == 1:
+        #         # locally set up the counts
+        #         nn = n
+        #         set_max = 1
+        #         while nn + 1 in nums:                    
+        #             set_max += 1
+        #             nn += 1
+
+        #         if global_set_max < set_max: 
+        #             global_set_max = set_max
+
+        # return global_set_max 
